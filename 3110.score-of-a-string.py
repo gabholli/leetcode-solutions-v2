3d2b1787ce3fca1beff50874cc6889e92ee5f1dc
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-12-20 22:13:08 -0800

    Update 3110.score-of-a-string.py

--- 3110.score-of-a-string.py
+++ 3110.score-of-a-string.py
@@ -7,5 +7,12 @@
 # @lc code=start
 class Solution(object):
     def scoreOfString(self, s):
+        score = 0
 
+        for i in range(1, len(s)):
+            score += abs(ord(s[i]) - ord(s[i - 1]))
+
+        return score
+
+    # O(n) T< O(1) S
         # @lc code=end
